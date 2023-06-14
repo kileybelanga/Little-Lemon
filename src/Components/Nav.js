0f69ad801df742c1c993f47faf14dbdbd7747c69
kileybelanga <119125742+kileybelanga@users.noreply.github.com>
2023-06-13 20:07:08 -0400

    adding files

---
+++ src/Components/Nav.js
@@ -0,0 +1,17 @@
+
+
+function Nav() {
+    return (
+      <Nav>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Menue</li>
+            <li>Reservations</li>
+            <li>Order Online</li>
+            <li>Login</li>
+        </ul>
+      </Nav>
+    );
+  }
+  export default Nav;
